1dd38f6060ff7a3bdc698bec0a641a6839bbed70
theboss17121999 <46495612+theboss17121999@users.noreply.github.com>
2024-01-27 22:21:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
+app.use(cors());
 
 app.get("/sum", (req, res) => {
     const a = parseInt(req.query.a);
